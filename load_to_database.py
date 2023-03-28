c2ff04d037c723e9d0b85a251f3db8da98772fbb
Lorenzo_Lazzari <lorenzo.lazzari98@gmail.com>
2023-03-28 14:07:35 +0200

    added files to build a function app and rearranged folders

--- load_to_database.py
+++ load_to_database.py
@@ -1,40 +1,32 @@
 from azure.cosmos import exceptions, CosmosClient, PartitionKey
 import config
-import datetime
 
-HOST = config.settings['host']
-MASTER_KEY = config.settings['master_key']
+def uploadDict (pythondict):
+    HOST = config.settings['host']
+    MASTER_KEY = config.settings['master_key']
+    client = CosmosClient(HOST, credential=MASTER_KEY)
+    database = client.get_database_client('data-es-job')
+    container = database.get_container_client('glassdoor_count')
+    container.create_item(body=pythondict)
+    return
 
-client = CosmosClient(HOST, credential=MASTER_KEY)
-
-database = client.get_database_client('data-es-jobs')
-container = database.get_container_client('infojobs_count')
-
+######################################################
 # a datastructure like we will need
 
-now = datetime.datetime.now()
-date_string = now.strftime("%Y-%m-%d")
+#now = datetime.datetime.now()
+#date_string = now.strftime("%Y-%m-%d")
 
-job_count_sample_entry = {
-    "id": date_string,
-    "data_engineer": 40,
-    "data_science": 30,
-    "data_analyst": 50,
-    "business_analyst": 60,
-    "area": "Barcelona" 
-    }
+#pythondict = {
+#    "id": date_string,
+#    "data_engineer": 40,
+#    "data_science": 30,
+#    "data_analyst": 50,
+#    "business_analyst": 60,
+#    "area": "Barcelona" 
+#    }
 
-container.create_item(body=salary_sample)
-container.delete_item(item="2023-03-17", partition_key="2023-03-17") #delete #partion key is sam eof id?
+#container.create_item(body=salary_sample)
+#container.delete_item(item="2023-03-17", partition_key="2023-03-17") #delete #partion key is sam eof id?
 
+#################################################################
 
-item_id = '<your_item_id>'
-
-# Construct the DELETE query
-query = "DELETE FROM c WHERE c.id = @item_id"
-
-# Set the query parameters
-query_params = [{'name': '@item_id', 'value': item_id}]
-
-# Execute the query
-result = container.query_items(query, parameters=query_params)
--- quickstart_sample_azure_codes/sql-python/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-settings = {
-    'host': os.environ.get('ACCOUNT_HOST', 'https://data-es-jobs.documents.azure.com:443/'),
-    'master_key': os.environ.get('ACCOUNT_KEY', 'ka24eMJnoFy7YV9jhaugR1MfjqhubUAU9EXv8IzQjyQ9nY4HJ7vAe5Cwht3LvGwSB6UcClPRMxmbACDbGeojkg=='),
-    'database_id': os.environ.get('COSMOS_DATABASE', 'ToDoList'),
-    'container_id': os.environ.get('COSMOS_CONTAINER', 'Items'),
-}
